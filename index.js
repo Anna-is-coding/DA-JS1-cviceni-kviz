44ed25bfdbab402aa79b412c00c95916f155f5a5
Anna Kršková <krskova.aaa@gmail.com>
2025-08-16 21:24:26 +0200

    Solved the task - quiz, right and wrong answers

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+const questionEl = document.querySelector('.question');
+const question =
+  'Uveďte příjmení amerického vynálezce, který v roce 1876 jako první úspěšně podal patent na zařízení, které je dnes považováno za první telefon.';
 
+questionEl.textContent = question;
+
+const userAnswer = prompt(question);
+
+const answerTextElm = document.querySelector('.answer__text');
+answerTextElm.textContent = userAnswer;
+
+const answerElm = document.querySelector('.answer');
+
+if (userAnswer === 'Bell') {
+  answerElm.classList.add('answer--correct');
+} else {
+  answerElm.classList.add('answer--wrong');
+}
